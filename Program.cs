b0e096803aad851a49f0c089c7dfd224c73fed70
Apoorva (Aps) Gupta <96203513+ApoorvaGuptaaa@users.noreply.github.com>
2025-05-06 15:13:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace bank
+{
+    public class Program
+    {
+        static void Pin()
+        {
+            int _min = 1000;
+            int _max = 9999;
+            Random _rdm = new Random();
+            int pin = _rdm.Next(_min, _max);
+            Console.WriteLine("Your Auto Generated PIN for ATM: " + pin);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Bank!!\n\nREGISTER YOURSELF");
+
+            Console.WriteLine("Enter Your Account Name: ");
+            string user = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Account Number: ");
+            long acno = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Date of Birth: ");
+            string dob = Console.ReadLine();
+
+            Console.WriteLine("Successful Registeration!! Congratulations!");
+            Pin();
+        }
+    }
+}
